3f9f32c3dcbf6e275aab824d655c79215c50420c
Francine N <hello@francine.codes>
2014-12-07 06:56:32 -0500

    created callback function to update DOM upon successful AJAX call

--- includes/_highlights.php
+++ includes/_highlights.php
@@ -1,6 +1,6 @@
 <section class="row highlights">
 <!-- Most recent -->
-	<div class="col-lg-4 col-md-6 col-xs-12 center highlight">
+	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="mostRecent">
 		<h3>Most Recent</h3>
 		<div class="video">
 		<!-- Thumbnail Image -->
@@ -8,12 +8,10 @@
 		</div>
 		<!-- Incident details -->
 		<h4 class="incident text-primary">Incident</h4>
-		<h4 class="location text-info">Location</h4>
-		<h4 class="date text-muted">Date</h4>
 		<a href="incident.php" class="view btn btn-info btn-sm">View</a>
 	</div>
 <!-- Highest rated -->
-	<div class="col-lg-4 col-md-6 col-xs-12 center highlight">
+	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="highest-rated">
 		<h3>Highest Rated</h3>
 		<div class="video">
 			<!-- Thumbnail Image -->
@@ -21,12 +19,10 @@
 		</div>
 		<!-- Incident details -->
 		<h4 class="incident text-primary">Incident</h4>
-		<h4 class="location text-info">Location</h4>
-		<h4 class="date text-muted">Date</h4>
 		<a href="incident.php" class="view btn btn-info btn-sm">View</a>
 	</div>
 <!-- Lowest rated -->
-	<div class="col-lg-4 col-md-6 col-xs-12 center highlight">
+	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="lowest-rated">
 		<h3>Lowest Rated</h3>
 		<div class="video">
 			<!-- Thumbnail Image -->
@@ -34,8 +30,6 @@
 		</div>
 		<!-- Incident details -->
 		<h4 class="incident text-primary">Incident</h4>
-		<h4 class="location text-info">Location</h4>
-		<h4 class="date text-muted">Date</h4>
 		<a href="incident.php" class="view btn btn-info btn-sm">View</a>
 	</div>
 </section>
