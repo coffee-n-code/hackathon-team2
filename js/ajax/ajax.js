118539f4cbb69afa26ca13eedd9b4e97a7b5cb53
Francine N <hello@francine.codes>
2014-12-07 07:37:22 -0500

    updated ajax

--- js/ajax/ajax.js
+++ js/ajax/ajax.js
@@ -1,14 +1,19 @@
 function displayData(data){
-	var iframe = '<iframe width="250" height="150" src="'+src+'" frameborder="0" allowfullscreen></iframe>';
 	$('#mostRecent').find('h4.incident').text(data[0].node_title);
 	$('#mostRecent').find('.video-thumbnail').attr('href', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
-	$('#mostRecent').find('iframe').attr('src', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	// $('#mostRecent').find('.video-thumbnail').html('<iframe width="250" height="150" src="'+data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url+'" frameborder="0" allowfullscreen></iframe>');
+	function constructSrc(index){
+		var src = data[index]['Evidence Piece'][0].field_media_url['und'][0].display_url.replace('https://www.youtube.com/watch?v=','');
+		return src;
+	};
+	
+	$('#mostRecent').find('.video-thumbnail').html('<iframe width="250" height="150" src="//www.youtube.com/embed/'+constructSrc(0)+'" frameborder="0" allowfullscreen></iframe>');
 	$('#highestRated').find('h4.incident').text(data[2].node_title);
 	$('#highestRated').find('.video-thumbnail').attr('href', data[2]['Evidence Piece'][0].field_media_url['und'][0].display_url);
-	$('#highestRated').find('iframe').attr('src', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#highestRated').find('.video-thumbnail').html('<iframe width="250" height="150" src="//www.youtube.com/embed/'+constructSrc(2)+'" frameborder="0" allowfullscreen></iframe>');
 	$('#lowestRated').find('h4.incident').text(data[1].node_title);
 	$('#lowestRated').find('.video-thumbnail').attr('href', data[1]['Evidence Piece'][0].field_media_url['und'][0].display_url);
-	$('#lowestRated').find('iframe').attr('src', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#lowestRated').find('.video-thumbnail').html('<iframe width="250" height="150" src="//www.youtube.com/embed/'+constructSrc(1)+'" frameborder="0" allowfullscreen></iframe>');
 };
 
 
