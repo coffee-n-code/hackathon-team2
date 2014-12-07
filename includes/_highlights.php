b31d88e4a3343925d487f7327497bd3ee0207ca6
Francine N <hello@francine.codes>
2014-12-06 23:51:39 -0500

    highlights.php

---
+++ includes/_highlights.php
@@ -0,0 +1,35 @@
+<section class="row highlights">
+	<div class="col-md-4">
+		<h3>Most Recent</h3>
+		<div class="video">
+		<!-- Thumbnail Image -->
+			<img src="http://placehold.it/250x150" alt="">
+		</div>
+		<h4 class="incident text-primary">Incident</h4>
+		<h4 class="location text-info">Location</h4>
+		<h4 class="date text-muted">Date</h4>
+		<button class="view btn btn-info btn-sm">View</button>
+	</div>
+	<div class="col-md-4">
+		<h3>Highest Rated</h3>
+		<div class="video">
+			<!-- Thumbnail Image -->
+			<img src="http://placehold.it/250x150" alt="">
+		</div>
+		<h4 class="incident text-primary">Incident</h4>
+		<h4 class="location text-info">Location</h4>
+		<h4 class="date text-muted">Date</h4>
+		<button class="view btn btn-info btn-sm">View</button>
+	</div>
+	<div class="col-md-4">
+		<h3>Lowest Rated</h3>
+		<div class="video">
+			<!-- Thumbnail Image -->
+			<img src="http://placehold.it/250x150" alt="">
+		</div>
+		<h4 class="incident text-primary">Incident</h4>
+		<h4 class="location text-info">Location</h4>
+		<h4 class="date text-muted">Date</h4>
+		<button class="view btn btn-info btn-sm">View</button>
+	</div>
+</section>
