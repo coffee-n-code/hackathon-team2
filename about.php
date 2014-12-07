b364d39a65a5b50dba4796b1b2a5a3b4fb797ece
sqsinger <sqsinger1123@gmail.com>
2014-12-07 05:52:40 -0500

    updated about us

--- about.php
+++ about.php
@@ -12,7 +12,8 @@
 					<h1>About Us</h1>
 					<p>The goal of Good Cop Bad Cop is to bring greater accountability to police forces as well as the communities they serve by fostering
 						positive connections and honest communication. We believe in the power of storytelling to reveal the otherwise unseen narratives of interactions between citizens and 
-						law enforcement officers. With the actions of police officers, as well as citizens, being so often called into question</p>
+						law enforcement officers. With the actions of police officers, as well as citizens, being so often called into question, our platform seeks to reconcile discrepancies
+						between varying stories of similar incidents to support peaceful conflict resolution.</p>
 					<p>Good Cop Bad Cop started at a hackathon, an all-night coding event at the Centre for Social Innovation.</p>
 				</div>
 				<div class="col-sm-5">
