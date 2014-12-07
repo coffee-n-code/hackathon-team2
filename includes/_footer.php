a8377cbe7909385e9111eae630974af0a7f6daa5
Francine N <hello@francine.codes>
2014-12-07 05:47:43 -0500

    additional styling

--- includes/_footer.php
+++ includes/_footer.php
@@ -1,6 +1,6 @@
 	<div id="footer" class="container-fluid clearfix">
 		<div class="footer row clearfix">
-			<div id="copyright">Copyright <?php echo date("Y"); ?></div>
+			<div id="copyright">Copyright &copy; <?php echo date("Y"); ?></div>
 
 			<a class="footer_link"	href="about.php" target="_top">About Us</a>
 
