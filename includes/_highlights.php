5950ea6ec6e81d6e574ceacd106bb192ac6285ca
Francine N <hello@francine.codes>
2014-12-07 07:24:28 -0500

    further changes to frontend callback function

--- includes/_highlights.php
+++ includes/_highlights.php
@@ -4,29 +4,29 @@
 		<h3>Most Recent</h3>
 		<div class="video">
 		<!-- Thumbnail Image -->
-			<img src="http://placehold.it/250x150" alt="">
+			<a href="" class="video-thumbnail" target="_blank"><iframe width="250" height="150" src="//www.youtube.com/embed/IeHBc3W4Raw" frameborder="0" allowfullscreen></iframe></a>
 		</div>
 		<!-- Incident details -->
 		<h4 class="incident text-primary">Incident</h4>
 		<a href="incident.php" class="view btn btn-info btn-sm">View</a>
 	</div>
 <!-- Highest rated -->
-	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="highest-rated">
+	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="highestRated">
 		<h3>Highest Rated</h3>
 		<div class="video">
 			<!-- Thumbnail Image -->
-			<img src="http://placehold.it/250x150" alt="">
+			<a href="" class="video-thumbnail" target="_blank"></a>
 		</div>
 		<!-- Incident details -->
 		<h4 class="incident text-primary">Incident</h4>
 		<a href="incident.php" class="view btn btn-info btn-sm">View</a>
 	</div>
 <!-- Lowest rated -->
-	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="lowest-rated">
+	<div class="col-lg-4 col-md-6 col-xs-12 center highlight" id="lowestRated">
 		<h3>Lowest Rated</h3>
 		<div class="video">
 			<!-- Thumbnail Image -->
-			<img src="http://placehold.it/250x150" alt="">
+			<a href="" class="video-thumbnail" target="_blank"><iframe width="250" height="150" src="//www.youtube.com/embed/IeHBc3W4Raw" frameborder="0" allowfullscreen></iframe></a>
 		</div>
 		<!-- Incident details -->
 		<h4 class="incident text-primary">Incident</h4>
--- js/ajax/ajax.js
+++ js/ajax/ajax.js
@@ -1,6 +1,14 @@
 function displayData(data){
-	$('#mostRecent').find('h4.incident').text(data.node_title);
-	$('#videoTitle').text(data.node_title);
+	var iframe = '<iframe width="250" height="150" src="'+src+'" frameborder="0" allowfullscreen></iframe>';
+	$('#mostRecent').find('h4.incident').text(data[0].node_title);
+	$('#mostRecent').find('.video-thumbnail').attr('href', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#mostRecent').find('iframe').attr('src', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#highestRated').find('h4.incident').text(data[2].node_title);
+	$('#highestRated').find('.video-thumbnail').attr('href', data[2]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#highestRated').find('iframe').attr('src', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#lowestRated').find('h4.incident').text(data[1].node_title);
+	$('#lowestRated').find('.video-thumbnail').attr('href', data[1]['Evidence Piece'][0].field_media_url['und'][0].display_url);
+	$('#lowestRated').find('iframe').attr('src', data[0]['Evidence Piece'][0].field_media_url['und'][0].display_url);
 };
 
 function incident_ajax_callback(data){
