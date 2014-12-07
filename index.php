981a22ea57c2c92802f926a25a2e355b2593018d
sqsinger <sqsinger1123@gmail.com>
2014-12-06 23:45:36 -0500

    common include files coming into place

--- index.php
+++ index.php
@@ -4,36 +4,14 @@
 
 		<title>Police Accountability</title>
 
-		<!-- Favicon with fallbacks for cross-browser -->
-		<link rel='shortcut icon' type='image/x-icon' href="/favicon.ico">
-		<link rel='icon' type='image/x-icon' href="/favicon.ico">
-
-		<!-- Stylesheets -->
-		<link rel="stylesheet" type="text/css" href="/bootstrap-3.1.1-dist/css/bootstrap.min.css" />
-
-		<!-- Javascript stuff -->
-		<script type="text/javascript" src="/bootstrap-3.1.1-dist/js/bootstrap.min.js"></script>
-		<script type="text/javascript" src="/js/vendor/jquery.min.js"></script>
-
-
-
-	</head>
-	<body>
-		<!-- A silly and standard IE warning -->
-		<!--[if lte IE 7]>
-			<h1>Whoa, you're using an old Internet Explorer.<br/>
-			Sorry, this site isn't optimized for your older browser. Please <a href="http://browsehappy.com/">update your browser</a> for the best experience.</h1>
-		<![endif]-->
-
-		<!-- The header of our site -->
+		<?php include("includes/_header.php"); ?>
 
 		<div class="container-fluid">
 			<div class="row">
 				<div class="col-xs-12">
-					<h1>Police Accountability</h1>
+					<h1>Good Cop Bad Cop</h1>
 					<p>Hackathon team 2: very basic starting site</p>
 
-					We are set up with Twitter Bootstrap, jQuery, and jQueryUI, ready to make a flashy website!
 				</div>
 			</div>
 		</div>
