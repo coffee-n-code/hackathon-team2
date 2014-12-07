a0a86cb2636c523de0374303a166fbcf2b3847d4
Francine N <hello@francine.codes>
2014-12-07 05:11:28 -0500

    error message

--- js/ajax/ajax.js
+++ js/ajax/ajax.js
@@ -2,9 +2,14 @@ $(function(){
 	$.ajax({
 		url: 'http://goodcopbadcop.co/en/api/v1/incident_service.json',
 		type: 'GET',
-		dataType: 'jsonp',
-		success: function(){
+		dataType: "jsonp",
+		error: function(result,a,b){
+			console.log(":(");
+			console.log(result);
+		},
+		success: function(result,a,b){
 			console.log("Huzzah!");
+			console.log(result);
 		}
 	});
 });
